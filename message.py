019ce2c23c9c6aab58f36d7fba6533e744a6df54
Rumok <rumko@ya.ru>
2025-10-27 11:59:01 +0300

    чуть больше по строкам

--- message.py
+++ message.py
@@ -1,2 +1,12 @@
 message = 'hw'
 print(message)
+name = 'ada lovelace'
+print(name.title()) #вывод с большими первыми буквами каждого слова в строке
+print(name.upper()) #вывод строки большими  буквами
+print(name.lower()) #вывод строки маленькими  буквами
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = first_name + ' ' + last_name #конкатенация
+print(full_name)
+#for iter in range(1000):   # символы юникода по их цифровым значениям
+#    print(str(iter) + ' ' + chr(iter) + '\n')
